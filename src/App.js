b19902d51de556a3c3727dd71a3a8f1a448c9bba
SangDoan <doansang654@gmail.com>
2020-10-28 20:25:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Boards from './components/Boards';
+
+function App() {
+  return (
+      <div className="App">
+        <Boards></Boards>
+    </div>
+  );
+}
+
+export default App;
